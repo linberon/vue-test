8f8e2058a755124c0f369b2552b87d4a6749a385
linberon <linberon@gmail.com>
2018-05-06 14:26:32 +0100

    update

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+Vue.component("shop-list", {
+	template: `
+	<div>
+	<list v-for="item in items">{{item.description}}</list>
+	</div>`,
+
+	data() {
+		return {
+			items: [
+				{ description: "pineapple", complete: true },
+				{ description: "coconut", complete: true },
+				{ description: "mango", complete: false },
+				{ description: "banana", complete: true }
+			]
+		};
+	}
+});
+
 Vue.component("list", {
-	template: "<li>Something</li>"
+	template: "<li><slot></slot></li>"
 });
 
 new Vue({
